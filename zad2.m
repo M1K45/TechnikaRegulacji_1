b90f0b666c1ddd60c6cd790704f64cbbe81e5bb8
M1K45 <118308157+M1K45@users.noreply.github.com>
2024-04-29 21:23:34 +0200

    Add files via upload
    Poprawa zadania drugiego

--- zad2.m
+++ zad2.m
@@ -1,9 +1,14 @@
-syms s;
+syms s t;
 
 F_3 = (s^3 + 4*s^2 + 6*s + 5) / ((s+8) * (s^2 + 8*s + 3) * (s^2 + 5*s + 7));
 
-f_3 = ilaplace(F_3)
+f_3 = ilaplace(F_3, s, t);
 
 pretty(simplify(f_3));
 
 fplot (f_3, [0, 50]);
+%xlim([0 50])
+xlim([0 5])
+xlabel('Wektor czasu')
+ylim([0 0.044])
+ylabel('Przebieg czasowy')
